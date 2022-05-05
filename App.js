df289cfde1dc7d1f763f0b9bce74f66aeb1b4866
Anthony <anthonynwobodo7@gmail.com>
2022-05-05 12:57:06 +0100

    :sparkles: (Template setup completed)

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { 
-  Text, 
-  View
- } from 'react-native';
- import NavigationContainer from './src/navigation/Stack';
+import AppLoading from 'expo-app-loading';
+import { func } from './src/constants';
+import NavigationContainer from './src/navigation/Stack';
 
 export default function App() {
+
+
+  const [currentSongData, setCurrentSongData] = useState({
+        album: 'Swimming',
+        artist: 'Mac Miller',
+        image: 'swimming',
+        length: 312,
+        title: 'So It Goes'
+      })
+  const [toggleTabBar, setToggleTabBar] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  if (isLoading) {
+      return (
+        <AppLoading
+          onError={() => {
+            // console.warn
+          }}
+          onFinish={() => setIsLoading(false)}
+          startAsync={func.loadAssetsAsync}
+        />
+      );
+    }
+
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <NavigationContainer />
+      <NavigationContainer screenProps={{
+            currentSongData,
+            changeSong: setCurrentSongData,
+            toggleTabBarState: toggleTabBar,
+            setToggleTabBar: setToggleTabBar
+          }} />
     </>
     
   );
