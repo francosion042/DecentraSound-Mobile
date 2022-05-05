c4ff5b44a873a945bda2674434aaa50ee2a10591
Anthony <anthonynwobodo7@gmail.com>
2022-05-05 11:07:58 +0100

    :sparkles: (navigation set up)

---
+++ src/navigation/TabNavigation.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { colors } from '../constants';
+
+// navigation stacks
+import StackHome from './StackHome';
+import StackSearch from './StackSearch';
+import StackLibrary from './StackLibrary';
+
+// components
+import CustomTabBar from '../components/CustomTabBar';
+
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    StackHome,
+    StackSearch,
+    StackLibrary
+  },
+  {
+    initialRouteName: 'StackHome',
+    tabBarComponent: (props) => <CustomTabBar {...props} />,
+    tabBarOptions: {
+      activeTintColor: colors.white,
+      inactiveTintColor: colors.greyInactive,
+      style: {
+        backgroundColor: colors.grey,
+        borderTopWidth: 0
+      }
+    }
+  }
+);
+
+export default BottomTabNavigator;
