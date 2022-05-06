a9e0339413bfe423067b3096d8803a1706bf0e40
Anthony <anthonynwobodo7@gmail.com>
2022-05-06 23:44:03 +0100

    :sparkles: (wallet connection )

---
+++ src/screens/authentication/WalletConnection.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class WalletConnection extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WalletConnection</Text>
+      </View>
+    );
+  }
+}
+
+export default WalletConnection;
