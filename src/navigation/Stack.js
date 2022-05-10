82dc01328773bb87a440f586103696027c04707b
Anthony <anthonynwobodo7@gmail.com>
2022-05-10 16:29:51 +0100

    :recycle: (structure change)

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -6,7 +6,7 @@ import TabNavigation from "./TabNavigation";
 
 // screens
 import ModalMusicPlayer from "../screens/playing/ModalMusicPlayer";
-import ModalMoreOptions from "../screens/playing/ModalMoreOptions";
+import ModalMoreOptions from "../screens/utils/ModalMoreOptions";
 import ModalAccountOptions from "../screens/account/ModalAccountOptions";
 
 const StackNavigator = createStackNavigator(
